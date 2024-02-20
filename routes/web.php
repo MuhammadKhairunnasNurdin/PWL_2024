5211b68bd704e43e7e7b096522c040eaf7ef4fce
NaufalSyahandra <114036607+NaufalSyahandra@users.noreply.github.com>
2024-02-20 11:19:21 +0700

    feat(Routing): Optional Routing

--- routes/web.php
+++ routes/web.php
@@ -53,3 +53,10 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
 Route::get('/articles/{id}', function ($id) {
     return "Halaman Artikel dengan ID: $id";
 });
+
+/**
+ * Optional Routing
+ */
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});

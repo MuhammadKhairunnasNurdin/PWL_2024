52f035bf2012360efab87237242d49357166dac0
NaufalSyahandra <114036607+NaufalSyahandra@users.noreply.github.com>
2024-02-20 10:10:27 +0700

    feat(Routing): Routing-basic

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/**
+ * Basic Routing
+ */
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+Route::get('/world', function () {
+    return 'World';
+});
+
+/**
+ * Use Case Basic Routing in Jobsheet
+ */
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+Route::get('/about', function () {
+    return 'NIM     : 2241720133'. '<br>'. 'Nama    : Muhammad Khairunnas Nurdin';
+});
